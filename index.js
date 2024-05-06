e1139375aecd3c678ac6440755e735f87466c588
Melanie <melanie.collins.p@gmail.com>
2024-05-06 18:26:55 -0400

    ruta POST/canción

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ import { Canciones } from './models/repertorio.model.js'
 const app = express()
 
 app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/cancion', async (req, res) => {
+
+app.get('/canciones', async (req, res) => {
     try {
         const canciones = await Canciones.findAll()
         return res.json(canciones)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(error)
+    }
+})
+
+app.post('/cancion', async (req, res) => {
+    try {
+        const { titulo, artista, tono } = req.body
+        const cancion = await Canciones.create({ titulo, artista, tono })
+        return res.json(cancion)
     } catch (error) {
         console.log(error)
         return res.status(500).json(error)
--- models/repertorio.model.js
+++ models/repertorio.model.js
@@ -5,6 +5,17 @@ const findAll = async () => {
     return rows
 }
 
+const create = async ({ titulo, artista, tono }) => {
+    const query = {
+        text: `INSERT INTO CANCIONES (TITULO, ARTISTA, TONO)
+        VALUES ($1, $2, $3)`,
+        values: [titulo, artista, tono]
+    }
+    const { rows } = await pool.query(query)
+    return rows[0]
+}
+
 export const Canciones = {
-    findAll
+    findAll,
+    create
 }
