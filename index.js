8a7454bec078b95edceade2826aee0e9941ebc8e
Melanie <melanie.collins.p@gmail.com>
2024-05-06 19:38:57 -0400

    modularizar

--- index.js
+++ index.js
@@ -1,55 +1,13 @@
 import express from 'express'
-import { Canciones } from './models/repertorio.model.js'
+import repertorioRoutes from './routes/repertorio.route.js'
+
 const app = express()
 
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
-app.get('/canciones', async (req, res) => {
-    try {
-        const canciones = await Canciones.findAll()
-        return res.json(canciones)
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json(error)
-    }
-})
-
-app.post('/cancion', async (req, res) => {
-    try {
-        const { titulo, artista, tono } = req.body
-        const cancion = await Canciones.create({ titulo, artista, tono })
-        return res.json(cancion)
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json(error)
-    }
-})
-
-app.delete('/cancion', async (req, res) => {
-    try {
-        const { id } = req.query
-        const cancion = await Canciones.remove(id)
-        return res.json(cancion)
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json(error)
-    }
-})
-
-app.put('/cancion/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { titulo, artista, tono } = req.body;
-        const cancion = await Canciones.update({ titulo, artista, tono, id })
-        return res.json(cancion)
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json(error)
-    }
-})
+app.use('/', repertorioRoutes)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
